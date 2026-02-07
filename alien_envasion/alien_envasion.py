97412e8aac39d09bb5080ef7b9f5f5c3a1dd1fb9
RealYns <imtherealyns@gmail.com>
2026-02-07 16:28:35 +0100

    new files

--- alien_envasion/alien_envasion.py
+++ alien_envasion/alien_envasion.py
@@ -16,7 +16,7 @@ def run_game():
     bullets = Group()
     aliens = Group()
 
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
 
 
@@ -24,6 +24,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
---
+++ alien_envasion/exercises/13_1/star.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Star(Sprite):
+
+
+    def __init__(self, stars_game):
+
+        super().__init__()
+        self.screen = stars_game.screen
+
+
+        self.image = pygame.image.load('../13_2/star.png')
+        self.rect = self.image.get_rect()
+
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+
+        self.y = float(self.rect.y)
